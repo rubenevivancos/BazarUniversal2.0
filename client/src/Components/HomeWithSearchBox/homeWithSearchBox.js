45b2d91d55c056fdc83e9b836633d45f6377e68c
rubenevivancos <rubenevivancos@hotmail.com>
2024-02-16 02:29:27 -0500

    Agregando componentes de Bootstrap

--- client/src/Components/HomeWithSearchBox/homeWithSearchBox.js
+++ client/src/Components/HomeWithSearchBox/homeWithSearchBox.js
@@ -29,6 +29,45 @@ export default function HomeWithSearchBox() {
 
     return(
         <div>
+            {/* NAVBAR INICIO */}
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            {/* NAVBAR FIN */}
+
+            {/* CARD INICIO */}
+            <div className="card" style={{ width: '18rem' }}>
+                <img src="..." className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            {/* CARD FIN */}
+
+
             <h1>Aprendiendo Bootstrap</h1>
             <button type="button" className="btn btn-primary">Shaka de Virgo</button>
         </div>
