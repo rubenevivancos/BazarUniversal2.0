a8625f30e86668d9cb3cb848dfefab7b7c93bf47
rubenevivancos <rubenevivancos@hotmail.com>
2024-02-19 23:30:27 -0500

    Modificando resultado de busqueda

--- client/src/Components/SearchResults/searchResults.js
+++ client/src/Components/SearchResults/searchResults.js
@@ -1,11 +1,23 @@
+import React, { useState } from "react";
 import { useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
+import { Container, Row, Col, Image, InputGroup, FormControl, Button } from 'react-bootstrap';
+
 
 import Product from "../Product/product.js";
-import "./searchResults.css";
+import imagen from '../../Images/carrito.png';
+import lupaIcon from '../../Images/lupa.png';
+
 
 
 export default function SearchResults() {
+
+    const [product, setProduct] = useState("");
+
+    const handleInput = (e) => {
+        e.preventDefault();
+        setProduct(e.target.value);        
+    }
 
     const listProducts = useSelector((state) => state.productReducer.listProduct);
     const productToSearch = useSelector((state) => state.productReducer.productToSearch);
@@ -14,21 +26,41 @@ export default function SearchResults() {
     if(listProducts.length){
         return(
             <>
-                <div>
-                    <div>
-                        <input type="text" placeholder="laptops, smartphones, ..."/>
-                        <div></div>
-                    </div>
-                </div>
-                <div>Resultados de la busqueda de " {productToSearch} ": {listProducts.length}</div>
-                <div className="category-list">
-                    {categoriesWithCount.map((cat) => (
-                        <div key={cat.category}>{cat.category} - {cat.count}</div>
-                    ))}
-                </div>
-                <div>
-                    { listProducts.map( product => <Link to={"/items/"+product.id} key={product.id}><Product product={product} /></Link>) }
-                </div>
+                <Container>
+                    <Row className="mb-1">
+                        <Col>
+                            <InputGroup className="mb-4">
+                                <Image src={imagen} style={{ width: "30px" }}/>
+                                <FormControl
+                                    placeholder="smartphones, laptops..."
+                                    aria-label="Búsqueda"
+                                    aria-describedby="basic-addon2"
+                                    onChange={handleInput}
+                                />
+                                <img src={lupaIcon} alt="Lupa" />
+                            </InputGroup>
+                        </Col>
+                    </Row>
+                    <Row className="mb-4">
+                        <Col className="text-center">
+                            <div>Resultados de la busqueda de " {productToSearch} ": {listProducts.length}</div>
+                        </Col>
+                    </Row>
+                    <Row className="mb-4 text-center">
+                        <Col>
+                            {categoriesWithCount.map((cat) => (
+                                <span key={cat.category} className="bg-danger p-2 m-2">{cat.category} - {cat.count}</span>
+                            ))}
+                        </Col>
+                    </Row>
+                    <Row className="mb-4">
+                        <Col className="text-center">
+                            <div>
+                                { listProducts.map( product => <Link to={"/items/"+product.id} key={product.id}><Product product={product} /></Link>) }
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </>
         )
     }
