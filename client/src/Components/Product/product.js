06d62b5b2d2dc4426852e7d7540461c8c4843c5d
rubenevivancos <rubenevivancos@hotmail.com>
2024-02-22 20:31:59 -0500

    Implementacion de rating de productos con estrellas

--- client/src/Components/Product/product.js
+++ client/src/Components/Product/product.js
@@ -1,39 +1,10 @@
 import { Container, Row, Col, Image, InputGroup, FormControl } from 'react-bootstrap';
+import CalificacionEstrellas from './stars';
 
 
 
 export default function Product({product}) {
 
-    // Función para renderizar las estrellas según la calificación
-    const renderStars = () => {
-        let stars = [];
-
-        let integerRating = Math.floor(product.rating); // Parte entera de la calificación
-
-        // Obtenemos la parte decimal restando la parte entera al número original
-        let decimal = product.rating - integerRating;
-        // Multiplicamos por 100 para obtener un número entero (eliminando el punto decimal)
-        let decimalPart = decimal * 100;
-        decimalPart = Math.floor(decimalPart); //Obtenemos la parte entera
-
-
-        for (let i = 0; i < 5; i++) {
-            if (i < integerRating) {
-                stars.push(<span key={i}><span key={i} className="star-active">&#9733;</span><br key={i+1}/><br key={i+2}/></span>);
-            } else {
-                let decimalWidth = `${decimalPart}%`;
-                stars.push(
-                    <span key={i+1}>
-                        <span key={i} className="star-active" style={{ width: decimalWidth }}>
-                            &#9733;
-                        </span><br key={i+1}/><br key={i+2}/>
-                    </span>
-                );
-                break;
-            }
-        }
-        return stars;
-    };
 
     return(
         <Container className="my-4"> {/* Agregamos my-4 para agregar margen arriba y abajo */}
@@ -48,7 +19,7 @@ export default function Product({product}) {
                     <p>{product.description}</p>
                     <div>
                         <span>{product.price}</span>
-                        <span>{renderStars()}</span>
+                        <CalificacionEstrellas calificacion={product.rating} />
                     </div>
                 </Col>
             </Row>
