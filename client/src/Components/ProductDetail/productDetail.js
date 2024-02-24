613e6a5f108aba05ea63406cb4b0d1c9d2d079eb
rubenevivancos <rubenevivancos@hotmail.com>
2024-02-24 17:54:02 -0500

    Modificando el detalle

--- client/src/Components/ProductDetail/productDetail.js
+++ client/src/Components/ProductDetail/productDetail.js
@@ -1,10 +1,22 @@
-import React, { useEffect }  from "react";
+import React, { useEffect, useState }  from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { Container, Row, Col, Image, InputGroup, FormControl } from 'react-bootstrap';
+
 
 import { getProductDetail } from "../../Redux/Actions/productAction";
+import imagen from '../../Images/carrito.png';
+import lupaIcon from '../../Images/lupa.png';
+
 
 export default function ProductDetail() {
+
+    const [productToSearch, setProductToSearch] = useState("");
+
+    const handleInput = (e) => {
+        e.preventDefault();
+        setProductToSearch(e.target.value);        
+    }
 
     const dispatch = useDispatch();
     let { id } = useParams();
@@ -15,35 +27,25 @@ export default function ProductDetail() {
 
     const product = useSelector((state) => state.productReducer.productDetail);
 
-    const error = useSelector((state) => state.productReducer.error);
 
     if(product !== null){
         return(
             <Container fluid className="py-4 overflow-auto vh-90" style={{ maxWidth: '50vh' }}>
-                <div >
-                    <div >
-                        <input type="text" placeholder="laptops, smartphones, ..."/>
-                        <div></div>
-                    </div>
-                    <div>
-                        <img src={product.images[0]} alt={product.title}/>
-                    </div>
-                    <div>
-                        {product.title}
-                    </div>
-                    <div>
-                        {product.price}
-                    </div>
-                    <div>
-                        {product.rating}
-                    </div>
-                    <div>
-                        {product.description}
-                    </div>
-                    <div>
-                        <button>Comprar</button>
-                    </div>
-                </div>
+                <Row className="mb-4 justify-content-left align-items-left">
+                    <Col xs={12} md={10}>
+                        <InputGroup>
+                            <Image src={imagen} style={{ width: "50px" }}/>
+                            <InputGroup.Text> </InputGroup.Text>
+                            <FormControl
+                                placeholder="smartphones, laptops..."
+                                aria-label="Búsqueda"
+                                aria-describedby="basic-addon2"
+                                onChange={handleInput}
+                            />
+                            <Image src={lupaIcon} alt="Lupa"/>
+                        </InputGroup>
+                    </Col>
+                </Row>
             </Container>
         )
     }
